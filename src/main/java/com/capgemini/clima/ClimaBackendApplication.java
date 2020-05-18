2707e3da1f99a8f3efe4d1784b92e5432410da69
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2020-05-17 21:42:15 -0300

    Commit inicial. Arquivos recem criados.

---
+++ src/main/java/com/capgemini/clima/ClimaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.clima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/clima/ClimaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.clima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
